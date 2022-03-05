9d725d705926dcd978b142511396be10c8e0f79a
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-03-05 18:57:46 +0530

    COMMIT

---
+++ Junit/src/Junitannotationpg.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Junitannotationpg {
+	
+			@BeforeAll
+			static void setup(){
+				System.out.println("@BeforeAll executed");
+			}
+
+			@BeforeEach
+			void setupThis(){
+				System.out.println("@BeforeEach executed");
+			}
+
+			@Test
+		    void testCalcOne()
+			{
+				System.out.println("TEST ONE EXECUTED");
+				
+			}
+		    @Test
+		    void testCalcTwo()
+		   {
+				System.out.println("TEST TWO EXECUTED");
+				
+		    }
+
+			@AfterEach
+			void tearThis(){
+				System.out.println("@AfterEach executed");
+			}
+
+			@AfterAll
+			static void tear(){
+				System.out.println("@AfterAll executed");
+			}
+		}
+
+
