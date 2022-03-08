52d1feb6aa3e937fa43e73e8c8297f81f5fd9ace
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-03-08 18:05:19 +0530

    commit

---
+++ Junit/src/Hero.java
@@ -0,0 +1,22 @@
+
+public class Hero implements Actor{
+	public void act()
+	{
+		System.out.println("I can act");
+	}
+	public void speak()
+	{
+		System.out.println("I can speak");
+	}
+	public void comedy()
+	{
+		System.out.println("I can comedy");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
