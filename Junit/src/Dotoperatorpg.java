f91d4a152c96b45b67cac61e6e4408261c8d4734
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-03-08 18:55:10 +0530

    commit

---
+++ Junit/src/Dotoperatorpg.java
@@ -0,0 +1,25 @@
+
+public class Dotoperatorpg {
+
+	
+		
+			void show() {
+		        int i = 67;
+		        System.out.println("In show method: "+ i);
+		    }
+
+		    static boolean isGreater(int a, int b) {
+		        return a > b;
+		    }
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				Dotoperatorpg doe = new Dotoperatorpg();
+		        doe.show();
+		        System.out.println("Is 7>4: " + Dotoperatorpg.isGreater(7, 4));
+			}
+
+		
+
+	}
+
+
